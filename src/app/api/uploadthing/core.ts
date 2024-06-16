22e3bbd29cbbb475e41630c564acbff8065e8ff3
rayhan889 <raydev1604@gmail.com>
2024-06-16 22:49:49 +0700

    accept file max 10

--- src/app/api/uploadthing/core.ts
+++ src/app/api/uploadthing/core.ts
@@ -4,12 +4,10 @@ import { getServerSession } from "next-auth";
 
 const f = createUploadthing();
 
-const auth = (req: Request) => ({ id: "fakeId" }); // Fake auth function
-
 // FileRouter for your app, can contain multiple FileRoutes
 export const ourFileRouter = {
   // Define as many FileRoutes as you like, each with a unique routeSlug
-  imageUploader: f({ image: { maxFileSize: "4MB" } })
+  imageUploader: f({ image: { maxFileSize: "4MB", maxFileCount: 10 } })
     // Set permissions and file types for this FileRoute
     .middleware(async ({ req }) => {
       // This code runs on your server before upload
