70d4fd288242de696afe45f0938c2e70e69c99cb
rayhan889 <raydev1604@gmail.com>
2024-06-21 01:32:53 +0700

    Sidebar sticky

--- src/app/_components/layout/Sidebar.tsx
+++ src/app/_components/layout/Sidebar.tsx
@@ -58,14 +58,10 @@ const Sidebar = () => {
         </div>
       </div>
       <div className="col-span-1 mt-4 hidden h-full pr-4 md:pr-8 lg:block">
-        <div className="flex flex-col items-end">
+        <div className="sticky top-4 flex flex-col items-end">
           <div className="space-y-2 lg:w-[230px]">
             {itemLinks.map((item) => (
               <Link key={item.name} href={item.href}>
-                {/* mobile */}
-                <div className="relative flex h-14 w-14 cursor-pointer items-center justify-center rounded-lg p-4 hover:bg-slate-300 hover:bg-opacity-10 lg:hidden">
-                  {item.icon}
-                </div>
                 {/* desktop */}
                 <div className="relative hidden w-full cursor-pointer items-center gap-x-4 rounded-lg p-4 hover:bg-slate-300 hover:bg-opacity-10 lg:flex">
                   {item.icon}
