2cd6ab05806feb1d05983ba06a984555c944a89a
rayhan889 <raydev1604@gmail.com>
2024-06-24 06:14:11 +0700

    Add new query and fix getMemes query

--- src/server/queries.ts
+++ src/server/queries.ts
@@ -3,34 +3,96 @@ import { eq, inArray } from "drizzle-orm";
 import { db } from "~/server/db";
 import { images, posts, users } from "~/server/db/schema";
 
-export const getMemes = async () => {
-  const memePostsQuery = await db
-    .select({
-      id: posts.id,
-      description: posts.description,
-      author: users.fullName,
-      createdAt: posts.createdAt,
-    })
-    .from(posts)
-    .leftJoin(users, eq(posts.userId, "9305b1ad-4c32-4736-ba7d-e8428530f0e7"));
+export type MemePayload = {
+  description: string;
+  userId: string;
+  images: {
+    name: string;
+    url: string;
+  }[];
+};
 
-  const imagesQuery = await db
-    .select({
-      id: images.id,
-      name: images.name,
-      url: images.url,
-      postId: images.postId,
-    })
-    .from(images)
-    .where(
-      inArray(
-        images.postId,
-        memePostsQuery.map((meme) => meme.id),
-      ),
-    );
+// All memes
+export const getAllMemes = async () => {
+  try {
+    const memePostsQuery = await db
+      .select({
+        id: posts.id,
+        description: posts.description,
+        author: users.fullName,
+        createdAt: posts.createdAt,
+      })
+      .from(posts)
+      .leftJoin(users, eq(posts.userId, users.id));
 
-  return memePostsQuery.map((meme) => ({
-    ...meme,
-    image: imagesQuery.filter((image) => image.postId === meme.id),
-  }));
+    if (memePostsQuery.length === 0) {
+      return [];
+    }
+
+    const imagesQuery = await db
+      .select({
+        id: images.id,
+        name: images.name,
+        url: images.url,
+        postId: images.postId,
+      })
+      .from(images)
+      .where(
+        inArray(
+          images.postId,
+          memePostsQuery.map((meme) => meme.id),
+        ),
+      );
+
+    return memePostsQuery.map((meme) => ({
+      ...meme,
+      image: imagesQuery.filter((image) => image.postId === meme.id),
+    }));
+  } catch (error) {
+    console.error("Error fetching memes:", error);
+    throw error;
+  }
+};
+
+// User posted memes only
+export const getMemes = async (userId?: string) => {
+  try {
+    const memePostsQuery = await db
+      .select({
+        id: posts.id,
+        description: posts.description,
+        author: users.fullName,
+        createdAt: posts.createdAt,
+      })
+      .from(posts)
+      .leftJoin(users, eq(posts.userId, users.id))
+      .where(eq(posts.userId, userId ?? ""));
+
+    if (memePostsQuery.length === 0) {
+      return [];
+    }
+
+    const imagesQuery = await db
+      .select({
+        id: images.id,
+        name: images.name,
+        url: images.url,
+        postId: images.postId,
+      })
+      .from(images)
+      .where(
+        inArray(
+          images.postId,
+          memePostsQuery.map((meme) => meme.id),
+        ),
+      );
+
+    return memePostsQuery.map((meme) => ({
+      ...meme,
+      image: imagesQuery.filter((image) => image.postId === meme.id),
+    }));
+  } catch (error) {
+    console.error("Error fetching memes:", error);
+    throw error;
+  }
 };
